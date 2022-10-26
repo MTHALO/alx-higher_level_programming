6225a8081e21234279320f474805463911eebcf9
MTHALO <mohammed.tegegne.35@gmail.com>
2022-10-26 13:16:49 -0700

    Python - Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+[2;2R[>77;30601;0c]10;rgb:f8f8/f8f8/f2f2]11;rgb:2828/2a2a/3636#!/usr/bin/python3
+"""  New class """
+
+
+class MyList(list):
+    """ Mylist class that inherist from list """
+
+    def print_sorted(self):
+        """ Fucntion that prints a sorted list """
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+[2;2R[>77;30601;0c]10;rgb:f8f8/f8f8/f2f2]11;rgb:2828/2a2a/3636#!/usr/bin/python3
+""" Exact same object """
+
+
+def is_same_class(obj, a_class):
+    """ Returns True if the object is exactly an instance
+    of the specified class """
+    return (type(obj) == a_class)
